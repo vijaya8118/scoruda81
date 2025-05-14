0324db3e97ecf9788801efe6f8aaeab37df812b5
vijaya8118 <140912151+vijaya8118@users.noreply.github.com>
2025-05-14 10:27:22 +0530

    Update settings.py

--- scoprj/settings.py
+++ scoprj/settings.py
@@ -196,7 +196,8 @@ STORAGES = {
     },
 }
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
