a0b3298e8ab113c00a89540bd76e0ce8f01a9acd
vijaya8118 <140912151+vijaya8118@users.noreply.github.com>
2025-08-18 20:39:43 +0530

    Update settings.py

--- scoprj/settings.py
+++ scoprj/settings.py
@@ -142,7 +142,7 @@ WSGI_APPLICATION = 'scoprj.wsgi.application'
 
 DATABASES = {
      'default': dj_database_url.parse(
-         config('DATABASE_URL', default='postgresql://scoruda_db_9aga_user:LdvIV0A9Bx1Kibn4fZkvnEus12YNIMCK@dpg-d2haun0dl3ps7384dam0-a/scoruda_db_9aga')
+         config('DATABASE_URL', default='postgresql://scoruda_db_d432_user:OtBicfjlHWUgYudvmblKeL0MiKJQUYmY@dpg-d2hk3gili9vc739euhv0-a/scoruda_db_d432')
      )
  }
 
